d8dd5d111fe21e6396efbf9fd24c9ed29aac065e
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2024-05-06 17:43:45 +0100

    Update KJW_MedicalExpansion.hpp
    Diseases

--- addons/core/KJW_MedicalExpansion.hpp
+++ addons/core/KJW_MedicalExpansion.hpp
@@ -29,5 +29,59 @@ class MOD_NAME {
 		class platelet {
 			displayName = "Platelets";
 		};
+		class flu {
+			displayName = "Influenza";
+		};
+		class cholera {
+			displayName = "Cholera";
+		};
+		class aids {
+			displayName = "AIDS";
+		};
+		class cold {
+			displayName = "Common Cold";
+		};
+		class ebola {
+			displayName = "Ebola";
+		};
+		class anthrax {
+			displayName = "Anthrax";
+		};
+		class malaria {
+			displayName = "Malaria";
+		};
+		class covid {
+			displayName = "COVID-19";
+		};
+		class plague {
+			displayName = "Bubonic Plague";
+		};
+		class vaccine_flu {
+			displayName = "Influenza Vaccine";
+		};
+		class vaccine_cholera {
+			displayName = "Cholera Vaccine";
+		};
+		class vaccine_aids {
+			displayName = "AIDS Vaccine";
+		};
+		class vaccine_cold {
+			displayName = "Common Cold Vaccine";
+		};
+		class vaccine_ebola {
+			displayName = "Ebola Vaccine";
+		};
+		class vaccine_anthrax {
+			displayName = "Anthrax Vaccine";
+		};
+		class vaccine_malaria {
+			displayName = "Malaria Vaccine";
+		};
+		class vaccine_covid {
+			displayName = "COVID-19 Vaccine";
+		};
+		class vaccine_plague {
+			displayName = "Bubonic Plague Vaccine";
+		};
 	};
 };
