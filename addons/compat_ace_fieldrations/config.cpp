6346d56893e7d31448a51399170009db2feae29c
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2024-10-25 20:20:28 +0100

    Make blood drinkable in October
    It's vampire time

---
+++ addons/compat_ace_fieldrations/config.cpp
@@ -0,0 +1,59 @@
+#include "script_component.hpp"
+
+#if __MONTH__ != 10
+#if __DAY__ < 28
+#define PATCH_SKIP "spooky szn"
+#endif
+#endif
+
+#ifdef PATCH_SKIP
+class CfgPatches {
+    class KJW_Overwrites_NotLoaded {
+        author="KJW";
+        requiredAddons[]=
+        {
+            "A3_Data_F",
+            "A3_Weapons_F",
+            "A3_Characters_F",
+            "A3_Data_F_AoW_Loadorder"
+        };
+        requiredVersion=1;
+		units[] = {};
+		weapons[] = {};
+		vehicles[] = {};
+        not_halloween = 1;
+    };
+};
+#else
+class CfgPatches {
+    class COMPONENT_GENERAL {
+        author="KJW";
+        requiredAddons[]=
+        {
+            "A3_Data_F",
+            "A3_Weapons_F",
+            "A3_Characters_F",
+            "A3_Data_F_AoW_Loadorder"
+        };
+        requiredVersion=1;
+		units[] = {};
+		weapons[] = {};
+		vehicles[] = {};
+	};
+};
+
+//https://trello.com/b/xH9BBZsd/kjws-medical-expansion
+
+class CfgWeapons {
+    class ACE_bloodIV;
+    class KJW_MedicalExpansion_bloodSample: ACE_bloodIV {
+        ACE_isFieldRationItem = 1;
+        acex_field_rations_consumeAnims[] = {"ace_field_rations_drinkStand","ace_field_rations_drinkCrouch","ace_field_rations_drinkProne"};
+        acex_field_rations_consumeSounds[] = {"ace_field_rations_drink1","ace_field_rations_drink1","ace_field_rations_drink2"};
+        acex_field_rations_consumeText = "Drinking from %1...";
+        acex_field_rations_consumeTime = 10;
+        acex_field_rations_thirstQuenched = 10;
+        acex_field_rations_replacementItem = "";
+    };
+};
+#endif
---
+++ addons/compat_ace_fieldrations/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT compat_ace_fieldrations
+#include "\y\KJW_MedicalExpansion\addons\core\major_includes.hpp"
+
+#define COMPONENT_BEAUTIFIED MOD_NAME_BEAUTIFIED Compat (ACE Field Rations)
