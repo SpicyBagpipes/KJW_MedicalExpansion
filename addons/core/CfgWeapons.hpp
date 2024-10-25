5feef1e2e86b4d75e6cff7012bf4b50b0ccc3a32
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2024-10-25 20:20:45 +0100

    Fix blood sample mass

--- addons/core/CfgWeapons.hpp
+++ addons/core/CfgWeapons.hpp
@@ -1,14 +1,17 @@
 class CfgWeapons {
+	class ACE_ItemCore;
+	class CBA_MiscItem_ItemInfo;
+
     class ACE_bloodIV_250;
     class ACE_bloodIV_500;
-    class ACE_bloodIV;
+    class ACE_bloodIV: ACE_ItemCore {
+		class ItemInfo;
+	};
     #include "fluids\blood_250ml\CfgWeapons.hpp"
     #include "fluids\blood_500ml\CfgWeapons.hpp"
     #include "fluids\blood_1000ml\CfgWeapons.hpp"
 	#include "fluids\blood_sample\CfgWeapons.hpp"
 
-	class ACE_ItemCore;
-	class CBA_MiscItem_ItemInfo;
 	class KJW_MedicalExpansion_500_TransfusionKit: ACE_ItemCore {
 		scope = 2;
 		displayName = "Empty Blood Bag (500ml)";
--- addons/core/fluids/blood_sample/CfgWeapons.hpp
+++ addons/core/fluids/blood_sample/CfgWeapons.hpp
@@ -2,5 +2,8 @@ class KJW_MedicalExpansion_bloodSample: ACE_bloodIV {
 	displayName = "Blood Sample (10ml)";
 	scope = 1;
 	picture = QPATHTOF(data\vial512full.paa);
+	class ItemInfo: ItemInfo {
+		mass = 0.01;
+	};
 };
 FLUID_IDS(KJW_MedicalExpansion_bloodSample,Blood Sample (10ml))
