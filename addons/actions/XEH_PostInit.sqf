ca9492d5744fc3de2b6f4d64e4d3bdd50fa6880a
Will/KJW <100206101+SpicyBagpipes@users.noreply.github.com>
2025-03-03 21:25:15 +0000

    Prepare for 3.19.1

--- addons/actions/XEH_PostInit.sqf
+++
@@ -1,1 +0,0 @@
-["KJW_MedicalExpansion_IV", "kat_IV_16"] call ace_common_fnc_registerItemReplacement;
--- addons/actions/config.cpp
+++ addons/actions/config.cpp
@@ -1,9 +1,5 @@
 #include "script_component.hpp"
 
-#if __has_include("\x\kat\addons\main\script_component.hpp")
-#define PATCH_SKIP "KAM Loaded"
-#endif
-
 class CfgPatches {
     class COMPONENT_GENERAL {
         author="KJW";
@@ -26,15 +22,5 @@ class CfgPatches {
 
 //https://trello.com/b/xH9BBZsd/kjws-medical-expansion
 
-#ifdef PATCH_SKIP
-#include "KAM_ACE_Medical_Treatment_Actions.hpp"
-#include "CfgWeapons.hpp"
-class Extended_PostInit_EventHandlers {
-	class COMPONENT_POSTINIT {
-		init = QUOTE(call compileScript [QQPATHTOF(XEH_PostInit.sqf)]);
-	};
-};
-#else
 #include "ACE_Medical_Treatment_Actions.hpp"
 #include "CfgWeapons.hpp"
-#endif
